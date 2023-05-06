5a1729f7522512bdb780c726aa931d45890f56a6
atis02 <guwancikramow@gmail.com>
2023-05-06 17:25:35 +0500

    ff

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,3 +13,19 @@ module.exports = {
 	},
 	plugins: [],
 };
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
+  },
+}
